5fcc32c87783cdd8285aa49bb6f6d6c08e7fe619
daniel <daniellsmarta@gmail.com>
2022-02-03 03:01:40 +0000

    crafting multiprocessing to accelerate computing

--- pl/multi_process_cartpole.py
+++ pl/multi_process_cartpole.py
@@ -1,3 +1,6 @@
+from threading import Thread, Lock
+from multiprocessing import Process, Pipe
+
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
@@ -9,6 +12,39 @@ import gym
 
 from oracle import HumanCritic
 from ppo import Buffer, PPO, logprobabilities
+
+
+class Environment(Process):
+
+    def __init__(self, env_idx, child_conn, env, state_size, action_size):
+        super(Environment, self).__init__()
+        self.env = env
+        self.env_idx = env_idx
+        self.child_conn = child_conn
+        self.state_size = state_size
+        self.action_size = action_size
+
+    def run(self):
+        super(Environment, self).run()
+        observation = self.env.reset()
+        # state = np.reshape(state, [1, self.state_size])
+        self.child_conn.send(state)
+        while True:
+            action = self.child_conn.recv()
+
+            observation, reward, done, _ = self.env.step(action)
+
+            # state = np.reshape(state, [1, self.state_size])
+
+            if done:
+                observation = self.env.reset()
+                # state = np.reshape(state, [1, self.state_size])
+
+            self.child_conn.send([observation, reward, done, _])
+
+
+# Multiprocessing
+num_workers = 4
 
 # Human Critic
 ask_human = True
@@ -29,8 +65,6 @@ target_kl = 0.01
 hidden_sizes = (64, 64)
 
 # True if you want to render the environment
-render = False
-render_frequency_episode = 100
 log_means_frequency = 50
 
 # Initialize the environment and get the dimensionality of the
@@ -39,19 +73,33 @@ env = gym.make("CartPole-v0")
 observation_dimensions = env.observation_space.shape[0]
 num_actions = env.action_space.n
 
-# Initialize the buffer
-buffer = Buffer(observation_dimensions, max_size_buffer)
+# Initialize HumanCritic
 human_critic = HumanCritic(obs_size=observation_dimensions, action_size=num_actions)
 
 # Initialize the actor and the critic as keras models
 ppo_agent = PPO(observation_dimensions, hidden_sizes, num_actions, policy_learning_rate, value_function_learning_rate,
                 clip_ratio, train_policy_iterations, train_value_iterations, target_kl)
 
+workers, parent_conns, child_conns = [], [], []
+for idx in range(num_workers):
+    parent_conn, child_conn = Pipe()
+    worker = Environment(idx, child_conn, env, observation_dimensions, num_actions)
+    buffer = Buffer(observation_dimensions, max_size_buffer)
+    worker.start()
+    workers.append(worker)
+    parent_conns.append(parent_conn)
+    child_conns.append(child_conn)
+
+observations_workers = [0 for _ in range(num_workers)]
+for worker_id, parent_conn in enumerate(parent_conns):
+    observations_workers[worker_id] = parent_conn.recv()
+
 # Initialize the observation, episode return and episode length
 observation, episode_return, episode_length = env.reset(), 0, 0
 
+episode = 0
 # Iterate over the number of epochs
-for episode in range(num_episodes):
+while episode < num_episodes:
     # Initialize the sum of the returns, lengths and number of episodes for each epoch
     observation = env.reset()
     episode_return, episode_length = 0, 0
@@ -63,8 +111,6 @@ for episode in range(num_episodes):
 
     # Iterate over the steps of each epoch
     while not done:
-        if render and episode % render_frequency_episode == 0:
-            env.render()
 
         # Get the logits, action, and take one step in the environment
         observation = observation.reshape(1, -1)
